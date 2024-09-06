d81dd1b41a52bd360ff71e26c1a2c617eab16bac
Radek Lefnar <radeklefnar@Radek--MacBook-Pro.local>
2024-09-06 12:29:52 +0200

    cleaned code

--- components/submit-btn.tsx
+++ components/submit-btn.tsx
@@ -10,7 +10,7 @@ export default function SubmitBtn({ pending }: SubmitBtnProps) {
         <button
             type="submit"
             className="group flex items-center justify-center gap-4 h-[3rem] w-[8rem] bg-slate-600 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-slate-300 active:scale-105 disabled:scale-100 disabled:bg-opacity-75"
-            disabled={pending} // Disable the button when pending is true
+            disabled={pending} 
         >
             {pending ? (
                 <div className="h-5 w-5 animate-spin rounded-full border-b-2 border-white"></div>
